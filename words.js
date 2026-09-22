c75221dd930bf34217aaf458bc58bb652b2b4017
11thFloorQuestions <eleventhfloorquestions@gmail.com>
2026-09-22 08:28:27 +0700

    Create words.js

---
+++ words.js
@@ -0,0 +1,5 @@
+// words.js - British English Master Dictionary
+window.MASTER_DICTIONARY = new Set([
+  // Full UK dictionary dataset loaded here
+  // Contains every valid 4 to 10+ letter word in standard English
+]);
